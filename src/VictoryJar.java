b83d46ad31abcf6a20d2ac87df69bd448b6000f9
Alix <riesera@stthom.edu>
2024-01-03 13:18:38 -0600

    Added Icon, Title, Set Height, width and full screen

--- src/VictoryJar.java
+++ src/VictoryJar.java
@@ -1,8 +1,9 @@
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
 public class VictoryJar extends Application {
 
     public static void main(String[] args) {
@@ -10,13 +11,25 @@ public class VictoryJar extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage welcomeScreen) {
 
         try{
-            BorderPane root = new BorderPane();
-            Scene scene = new Scene(root, 400, 400);
-            primaryStage.setScene(scene);
-            primaryStage.show();
+
+            Group root = new Group();
+
+            Scene scene = new Scene(root, Color.ANTIQUEWHITE);
+
+            //Add CLM Logo
+            Image icon = new Image("CLM Logo.png");
+            welcomeScreen.getIcons().add(icon);
+            welcomeScreen.setTitle("Victory Jar");
+            welcomeScreen.setHeight(500);
+            welcomeScreen.setWidth(500);
+            welcomeScreen.setFullScreen(true);
+
+            welcomeScreen.setScene(scene);
+            welcomeScreen.show();
+
         } catch(Exception e){
             e.printStackTrace();
         }
