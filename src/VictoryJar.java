d119b221e438df0acb59f6f05957b1b078f675b0
Alix <riesera@stthom.edu>
2024-01-10 12:07:11 -0600

    Added buttons and text via SceneBuilder to the welcome screen.

--- src/VictoryJar.java
+++ src/VictoryJar.java
@@ -1,8 +1,13 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
-import javafx.scene.Scene;
+import javafx.scene.Parent;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.Scene;
+
 import javafx.stage.Stage;
 public class VictoryJar extends Application {
 
@@ -15,19 +20,32 @@ public class VictoryJar extends Application {
 
         try{
 
-            Group root = new Group();
+            Parent root = FXMLLoader.load(getClass().getResource("welcomeScreen.fxml"));
 
             Scene scene = new Scene(root, Color.ANTIQUEWHITE);
 
+            //TODO: Add a nicer logo that's fit for purpose
             //Add CLM Logo
             Image icon = new Image("CLM Logo.png");
             welcomeScreen.getIcons().add(icon);
+
+            //Scene Setup
+            //TODO: Make it so if full screen is exited, text still looks correct
             welcomeScreen.setTitle("Victory Jar");
-            welcomeScreen.setHeight(500);
-            welcomeScreen.setWidth(500);
+            //welcomeScreen.setHeight(500);
+            //welcomeScreen.setWidth(500);
             welcomeScreen.setFullScreen(true);
 
-            welcomeScreen.setScene(scene);
+            //Text on Welcome Screen
+            Text welcomeText = new Text();
+            welcomeText.setText("Welcome to the Victory Jar Application!");
+            welcomeText.setX(125);
+            welcomeText.setY(100);
+            welcomeText.setFont(Font.font("SourceSans", 50));
+            welcomeText.setFill(Color.GREY);
+
+            //root.getChildren().add(welcomeText); //Add text
+            welcomeScreen.setScene(scene); //Add scene
             welcomeScreen.show();
 
         } catch(Exception e){
